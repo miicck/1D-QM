d86a22b62d8ae4a7851c627a7ebc9d93441ffcc4
miicck <michael.hutcheon@hotmail.co.uk>
2023-05-30 17:28:22 +0100

    Better plotting

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-from solver import schrodinger_1d, orbitals_from_density, density_from_orbitals
+from solver import orbitals_from_density, plot_orbital_density_potential_suite
 import numpy as np
 import matplotlib.pyplot as plt
 
 N = 5
 
-x = np.linspace(-10, 10, 1001)
+x = np.linspace(-5, 5, 101)
 
-rho = np.exp(-abs(x))
+rho = np.exp(-(x-1) ** 2) ** 2 + np.exp(-(x+1) ** 2) ** 2
+rho = np.exp(-(x-2) ** 2) ** 2 + np.exp(-x ** 2) ** 2 + np.exp(-(x+2) ** 2) ** 2
+#rho = np.exp(-x**2)**2
 rho *= N / np.trapz(rho, x)
 
-evals, phi, v = orbitals_from_density(x, rho, plot=True, n_elec_tol=0.001)
+evals, phi, v, history = orbitals_from_density(x, rho, plot=False, n_elec_tol=0.01)
 
-plt.subplot(221)
-plt.plot(x, rho, label="rho")
-plt.plot(x, density_from_orbitals(phi, N), label="rho orbs")
-plt.plot(x, v / max(abs(v)), label="v")
-plt.legend()
-
-plt.subplot(222)
-for i in range(N):
-    plt.plot(x, phi[:, i] + i)
-    plt.axhline(i, color="black", alpha=0.5)
-
-plt.subplot(223)
-phi_vw = (rho / N) ** 0.5
-plt.plot(x, phi[:, 0], label="phi_0")
-plt.plot(x, phi_vw, label="sqrt(rho)")
-plt.legend()
-
+plot_orbital_density_potential_suite(x, rho, phi, N, v, history)
 plt.show()
--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import math
-
+from typing import Dict
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse
@@ -80,6 +80,28 @@ def density_from_orbitals(orbs: np.ndarray, n_elec: float):
     return result
 
 
+def gradient(x: np.ndarray, orbital: np.ndarray) -> np.ndarray:
+    """
+    Returns the gradient of the given orbital d\phi/dx
+
+    Parameters
+    ----------
+    x: np.ndarray
+        coordinates
+    orbital: np.ndarray
+        orbital \phi
+
+    Returns
+    -------
+    grad: np.ndarray
+        Gradient of the given orbital
+    """
+    grad = np.zeros(orbital.shape)
+    for i in range(1, len(orbital)):
+        grad[i] = (orbital[i] - orbital[i - 1]) / (x[i] - x[i - 1])
+    return grad
+
+
 def laplacian(x: np.ndarray, orbital: np.ndarray) -> np.ndarray:
     """
     Returns the laplacian of the given orbital d^2\phi/dx^2.
@@ -99,8 +121,134 @@ def laplacian(x: np.ndarray, orbital: np.ndarray) -> np.ndarray:
     lap = np.zeros(orbital.shape)
     for i in range(2, len(orbital)):
         lap[i - 1] = orbital[i - 2] - 2 * orbital[i - 1] + orbital[i]
-    lap /= 2 * ((x[1] - x[0]) ** 2)
+
+    lap[0] = lap[1]
+    lap[-2] = lap[-1]
+
+    lap /= (x[1] - x[0]) ** 2
     return lap
+
+
+def plot_orbital_density_potential_suite(
+        x: np.ndarray,
+        rho: np.ndarray,
+        orbs: np.ndarray,
+        nelec: float,
+        v: np.ndarray,
+        history: Dict):
+    """
+    Generate the default suite of plots for an orbital/density/potential combination.
+
+    Parameters
+    ----------
+    x: np.ndarray
+        Coordinates
+    rho: np.ndarray
+        Density
+    orbs: np.ndarray
+        Orbitals
+    nelec: float
+        Number of electrons
+    v: np.ndarray
+        Potential
+    history: Dict
+        Optimization history
+
+    Returns
+    -------
+    None
+    """
+
+    def new_plot():
+        plt.subplot(3, 3, new_plot.n)
+        new_plot.n += 1
+
+    new_plot.n = 1
+
+    # Evaluate derived quantities
+    orb_g = (rho / np.trapz(rho, x))**0.5
+    orb_g = orbs[:, 0]
+    rho_phi = density_from_orbitals(orbs, nelec)
+
+    # Plot density
+    new_plot()
+    plt.plot(x, rho, label="rho")
+    plt.plot(x, rho_phi, label=f"rho_phi N = {np.trapz(rho_phi, x):.5f}")
+    plt.plot(x, v / max(abs(v)), label="v")
+    plt.legend()
+
+    # Plot orbitals
+    new_plot()
+    for i in range(math.ceil(nelec)):
+        orb = orbs[:, i]
+        orb /= max(orb) - min(orb)
+        plt.plot(x, orb + i)
+        plt.axhline(i, color="black", alpha=0.5)
+
+    orb = orb_g.copy()
+    orb /= max(orb) - min(orb)
+    plt.plot(x, orb, color="black", linestyle=":", label=r"$g$")
+
+    plt.legend()
+    plt.ylabel(r"$\phi_i$")
+
+    # Plot gradient of orbitals
+    new_plot()
+    for i in range(math.ceil(nelec)):
+        grad = gradient(x, orbs[:, i])
+        grad /= max(grad) - min(grad)
+        plt.plot(x, grad + i)
+        plt.axhline(i, color="black", alpha=0.5)
+    plt.ylabel(r"$d\phi_i/dx$")
+
+    # Plot laplacian of orbitals
+    new_plot()
+    for i in range(math.ceil(nelec)):
+        lap = laplacian(x, orbs[:, i])
+        lap /= max(lap) - min(lap)
+        plt.plot(x, lap + i)
+        plt.axhline(i, color="black", alpha=0.5)
+    plt.ylabel(r"$\nabla^2 \phi_i$")
+
+    # Plot successive gradients of vw orbital
+    new_plot()
+    grad = orb_g.copy()
+    ke_from_grads = 0.0
+    v_from_grads = 0.0
+    for i in range(math.ceil(nelec)):
+        # Normalize/eval KE
+        grad /= np.trapz(grad * grad, x) ** 0.5
+        ke_from_grads += -0.5 * np.trapz(laplacian(x, grad) * grad, x)
+        v_from_grads += np.trapz(grad * grad * v, x)
+
+        # Plot scaled
+        grad /= max(grad) - min(grad)
+        plt.plot(x, grad + i)
+        plt.axhline(i, color="black", alpha=0.5)
+        grad = gradient(x, grad)
+
+    plt.ylabel(r"$d^ng/dx^n$")
+    plt.annotate(f"KE = {ke_from_grads}\n"
+                 f"V  = {v_from_grads}", (0, 0.5))
+
+    new_plot()
+    plt.plot(history["E_T"], label="T")
+    plt.plot(history["E_V"], label="V")
+    plt.plot(history["E_tot"], label="Total")
+    plt.ylabel("Energy")
+    plt.yscale("symlog")
+    plt.xlabel("Iteration")
+    plt.legend()
+
+    new_plot()
+    plt.plot(x, v / rho_phi)
+    plt.ylabel("v/rho_phi")
+    plt.yscale("symlog")
+
+    new_plot()
+    plt.plot(x, orb_g / orbs[:, 0])
+    plt.ylabel(r"$g/\phi_0$")
+    plt.yscale("symlog")
 
 
 def orbitals_from_density(x: np.ndarray, rho: np.ndarray, plot: bool = False, n_elec_tol=0.1):
@@ -129,12 +277,15 @@ def orbitals_from_density(x: np.ndarray, rho: np.ndarray, plot: bool = False, n_
         Effective potential that generates the returned orbitals
     """
     if plot:
-        import matplotlib.pyplot as plt
         plt.ion()
 
     n_elec = np.trapz(rho, x)
     v = np.zeros(rho.shape)
-    kinetic_energies = []
+    history = {
+        "E_T": [],
+        "E_V": [],
+        "E_tot": []
+    }
 
     while True:
         evals, phi = schrodinger_1d(x, v)
@@ -142,39 +293,25 @@ def orbitals_from_density(x: np.ndarray, rho: np.ndarray, plot: bool = False, n_
         d_rho = rho_phi - rho
         v += d_rho
 
+        # Work out kinetic energy
+        history["E_T"].append(
+            sum(-0.5 * np.trapz(laplacian(x, phi[:, i]) * phi[:, i], x)
+                for i in range(math.ceil(n_elec)))
+        )
+
+        # Work out potential energy
+        history["E_V"].append(np.trapz(v * rho_phi, x))
+
+        # Work out total energy
+        history["E_tot"].append(history["E_V"][-1] + history["E_T"][-1])
+
         if np.trapz(abs(d_rho), x) < n_elec_tol:
             if plot:
                 plt.ioff()
                 plt.clf()
-            return evals, phi, v
+            return evals, phi, v, history
 
         if plot:
             plt.clf()
-
-            plt.subplot(221)
-            plt.plot(x, rho, label="rho")
-            plt.plot(x, rho_phi, label="rho_phi")
-            plt.plot(x, v / max(abs(v)), label="v")
-            plt.legend()
-
-            ke = 0.0
-
-            plt.subplot(222)
-            for i in range(math.ceil(n_elec)):
-                plt.plot(x, phi[:, i] + i)
-                plt.axhline(i, color="black", alpha=0.5)
-                ke += -0.5 * np.trapz(laplacian(x, phi[:, i]), x)
-
-            kinetic_energies.append(ke)
-
-            plt.subplot(223)
-            plt.plot(kinetic_energies)
-            plt.ylabel("Kinetic energy")
-            plt.xlabel("Iteration")
-
-            plt.subplot(224)
-            plt.plot(v / rho_phi)
-            plt.ylabel("v/rho_phi")
-            plt.yscale("symlog")
-
-            plt.pause(0.01)
+            plot_orbital_density_potential_suite(x, rho, phi, n_elec, v, history)
+            plt.pause(1e-6)
