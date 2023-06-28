7a8ead7717ce0304fe598cdbf89b9d352878cd33
miicck <michael.hutcheon@hotmail.co.uk>
2023-06-28 10:53:40 +0100

    Fixed tests

--- functions.py
+++ functions.py
@@ -1,7 +1,75 @@
+import math
+
 import numpy as np
 from qm1d.hilbert import *
 from qm1d.utils import assert_all_close
-from typing import List, Tuple
+from typing import List, Tuple, Callable, Iterable
+
+
+class Orbital(Function):
+    pass
+
+
+class OrbitalSpectrum:
+
+    def __init__(self, eigenvalues: Iterable[float], orbitals: Iterable[Orbital]):
+        self._eigenvalues = list(eigenvalues)
+        self._orbitals = list(orbitals)
+
+    @property
+    def eigenvalues(self) -> List[float]:
+        return self._eigenvalues
+
+    @property
+    def orbitals(self) -> List[Orbital]:
+        return self._orbitals
+
+    def density(self, particles: float) -> 'Density':
+        """
+        Evaluate the density that results from filling these
+        orbitals with the given number of particles (according to Aufbau scheme).
+
+        Parameters
+        ----------
+        particles: float
+            number of particles to put into these orbitals
+
+        Returns
+        -------
+        density: Density
+            Resulting particle density
+        """
+        result = Density(self.orbitals[0].x)
+        full = math.floor(particles)
+        result.values = sum(self.orbitals[i].values ** 2 for i in range(full))
+        result.values += (particles - full) * self.orbitals[full].values ** 2
+        return result
+
+
+class Potential(Function):
+
+    def calculate_eigenstates(self) -> OrbitalSpectrum:
+        # Evaluate the hamiltonian
+        h = -0.5 * self.x.laplacian + np.diag(self.values)
+        eigenvalues, eigenvectors = np.linalg.eigh(h)
+
+        orbitals = []
+        for i in range(self.x.points):
+
+            vals_i = eigenvectors[:, i]
+
+            # Ensure consistent sign of orbitals
+            for j in range(len(vals_i)):
+                if abs(vals_i[j]) > 0.0001:
+                    vals_i *= np.sign(vals_i[j])
+                    break
+
+            orb_i = Orbital(self.x)
+            orb_i.values = vals_i
+            orb_i.values /= orb_i.inner_product(orb_i) ** 0.5
+            orbitals.append(orb_i)
+
+        return OrbitalSpectrum(eigenvalues, orbitals)
 
 
 class Density(Function):
@@ -16,69 +84,57 @@ class Density(Function):
         self.values *= val
         assert_all_close(self.particles, val)
 
-    def calculate_potential(self) -> 'Potential':
-
-        import matplotlib.pyplot as plt
+    def calculate_potential(self, n_elec_tol: float = 0.01,
+                            callback: Callable[
+                                [Potential, OrbitalSpectrum, 'Density', 'Density'], None] = None) -> 'Potential':
 
         v = Potential(self.x)
         v.values = -self.values.copy()
 
-        plt.ion()
-
         while True:
+
+            # Diagonalize the potential
             eig = v.calculate_eigenstates()
 
-            eig_d = Density(self.x)
-
-            for i in range(5):
-                eig_d.values += eig[i][1].values ** 2
-                plt.plot(self.x.values, eig[i][1].values)
+            # Evaluate the resulting density
+            eig_d = eig.density(self.particles)
 
             assert_all_close(eig_d.particles, self.particles)
-
-            plt.plot(self.x.values, v.values / (max(v.values) - min(v.values)), color="red", linestyle=":")
-
-            plt.pause(0.01)
-            plt.clf()
-
-            plt.plot(self.x.values, eig_d.values, color="black")
-            plt.plot(self.x.values, self.values, color="black", linestyle=":")
-
             delta_density = eig_d.values - self.values
 
-            if np.trapz(abs(delta_density), self.x.values) < 0.01:
+            if np.trapz(abs(delta_density), self.x.values) < n_elec_tol:
                 break
 
             v.values += delta_density
+            v.values -= max(v.values)
 
-            plt.show()
+            if callback is not None:
+                callback(v, eig, self, eig_d)
 
+    @staticmethod
+    def calculate_potential_animation_callback(v: Potential, eig: OrbitalSpectrum, targ_d: 'Density', eig_d: 'Density'):
+        import matplotlib.pyplot as plt
+        plt.ion()
+        plt.clf()
 
-class Orbital(Function):
-    pass
+        plt.subplot(221)
+        plt.fill_between(v.x.values, v.values * 0, v.values)  #
+        plt.ylabel(r"$v$")
+        plt.xlabel(r"$x$")
 
+        plt.subplot(222)
+        for i in range(math.ceil(targ_d.particles)):
+            plt.plot(v.x.values, eig.orbitals[i].values)
+        plt.ylabel(r"$\phi$")
+        plt.xlabel(r"$x$")
 
-class Potential(Function):
+        plt.subplot(223)
+        plt.plot(v.x.values, eig_d.values, color="black", label=r"$\rho_\phi$")
+        plt.plot(v.x.values, targ_d.values, color="black", linestyle=":", label=r"$\rho$")
+        plt.ylabel(r"$\rho$")
+        plt.xlabel(r"$x$")
+        plt.legend()
 
-    def calculate_eigenstates(self) -> List[Tuple[float, Orbital]]:
-        # Evaluate the hamiltonian
-        h = -0.5 * self.x.laplacian + np.diag(self.values)
-        eigenvalues, eigenvectors = np.linalg.eigh(h)
+        plt.tight_layout()
 
-        result = []
-        for i in range(self.x.points):
-
-            vals_i = eigenvectors[:, i]
-
-            # Ensure consistent sign of orbitals
-            for j in range(len(vals_i)):
-                if abs(vals_i[j]) > 0.0001:
-                    vals_i *= np.sign(vals_i[j])
-                    break
-
-            orb_i = Orbital(self.x)
-            orb_i.values = vals_i
-            orb_i.values /= orb_i.inner_product(orb_i) ** 0.5
-            result.append((eigenvalues[i], orb_i))
-
-        return result
+        plt.pause(0.001)
--- test/test_functions.py
+++ test/test_functions.py
@@ -21,23 +21,25 @@ def test_density_particles():
 
 
 def test_potential_eigenstates():
-    v = Potential(Grid(-10, 10, 1000))
+    v = Potential(Grid(-20, 20, 1001))
     v.values = v.x.values ** 2
     eigs = v.calculate_eigenstates()
 
     # Check eigenvalues are correct
     omega = 2 ** 0.5
-    for i in range(0, 10):
-        assert np.allclose(eigs[i][0], omega * (0.5 + i), rtol=0.01)
+    evals = [omega * (0.5 + i) for i in range(len(eigs.eigenvalues))]
+
+    # Check the lowest-lying states are reproduced well
+    assert_all_close(eigs.eigenvalues[:20], evals[:20], rtol=0.01)
 
     # Check first eigenstate is correct
     exp = np.exp(-(v.x.values * omega ** 0.5) ** 2 / 2)
     exp /= np.trapz(exp * exp, v.x.values) ** 0.5
-    assert np.allclose(eigs[0][1].values, exp, rtol=0.01)
+    assert np.allclose(eigs.orbitals[0].values, exp, rtol=0.01)
 
 
 def test_density_potential():
-    d = Density(Grid(-10, 10, 1000))
-    d.values = np.exp(-d.x.values ** 2)
-    d.particles = 5.0
-    v = d.calculate_potential()
+    d = Density(Grid(-10, 10, 100))
+    d.values = np.exp(-(d.x.values - 1) ** 2) + np.exp(-(d.x.values + 1) ** 2)
+    d.particles = 3
+    v = d.calculate_potential(n_elec_tol=0.1)  # , callback=d.calculate_potential_animation_callback)
