61ce2b049361725858e575a4dfbc3c7826c0bde3
miicck <michael.hutcheon@hotmail.co.uk>
2023-07-04 18:19:49 +0100

    Added timer to profile_main

--- profile_main.py
+++ profile_main.py
@@ -4,7 +4,9 @@ from PIL import Image
 import cProfile
 from main import main
 
+start_time = time.time()
 cProfile.run("main()", "main_profile.out")
+print(f"Elapsed time = {time.time() - start_time}")
 os.system("wget -O gprof2dot.py https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py")
 os.system(f"python gprof2dot.py -f pstats main_profile.out | dot -Tpng -o main_profile.png")
 Image.open('main_profile.png').show()
