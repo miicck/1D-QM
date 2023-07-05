0a18ba57ce08600b4c2f3c9306729462545108be
miicck <michael.hutcheon@hotmail.co.uk>
2023-07-05 10:57:16 +0100

    Added time units

--- profile_main.py
+++ profile_main.py
@@ -6,7 +6,7 @@ from main import main
 
 start_time = time.time()
 cProfile.run("main()", "main_profile.out")
-print(f"Elapsed time = {time.time() - start_time}")
+print(f"Elapsed seconds = {time.time() - start_time}")
 os.system("wget -O gprof2dot.py https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py")
 os.system(f"python gprof2dot.py -f pstats main_profile.out | dot -Tpng -o main_profile.png")
 Image.open('main_profile.png').show()
